ca8007b6d3b0367d0824d514bf9bcc38a7e7b54c
Mayur Patil <mayurpatil1727@gmail.com>
2024-03-24 00:26:14 +0530

    another commit

--- numsandmath.js
+++ numsandmath.js
@@ -24,3 +24,7 @@ console.log(Math.ceil(4.2));
 console.log(Math.floor(4.2));
 
 console.log((Math.random()*10)+ 1);
+
+const min = 10 ;
+const max = 20 ;
+console.log((Math.random() * ((max - min)+ 1)));
