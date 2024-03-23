19a55092aea518becc8276a8bf7e65760971d1ed
Mayur Patil <mayurpatil1727@gmail.com>
2024-03-24 00:22:54 +0530

    math and nums

---
+++ numsandmath.js
@@ -0,0 +1,26 @@
+
+const score = 400 ;
+
+const balance = new Number(100);
+
+console.log(score);
+console.log(balance);
+
+console.log(balance.toFixed(3));
+
+const otherNum = 84.83463 ;
+
+console.log(otherNum.toPrecision(2));
+
+const hundreds =  1000000 ;
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+// =================================================================
+
+console.log(Math);
+console.log(Math.abs(-74));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+console.log((Math.random()*10)+ 1);
---
+++ strings.js
@@ -0,0 +1,7 @@
+
+const name = "mayur";
+const repo = 50 ;
+
+console.log(name + " " + repo);
+
+console.log(`Hello - Name :  ${name} , repo count : ${repo}`);
